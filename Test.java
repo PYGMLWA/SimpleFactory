40116f1a2f5a15207f316a289684d084b0347f8b
PL <1072336928@qq.com>
2017-03-05 13:01:49 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+package work02;
+
+public class Test {
+	
+	public static void main(String[] args){
+		
+		Nvwa nvwa =  new Nvwa();
+		nvwa.createPerson("M").speak();
+		nvwa.createPerson("W").speak();
+		nvwa.createPerson("R").speak();
+	}
+
+}
